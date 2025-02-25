e9ec9663863ace164f9bba7223360d4664758808
Your Name <your.email@example.com>
2025-02-25 21:25:18 +0300

    Default commit

---
+++ files/openTabs.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "tabs": [
+        [
+            {
+                "group": 1,
+                "label": "package.json",
+                "isActive": true,
+                "isPinned": false,
+                "relPath": "package.json"
+            },
+        ]
+    ],
+    "preview": {
+        "url": "http://localhost:3002/",
+        "group": 2
+    }
+}
